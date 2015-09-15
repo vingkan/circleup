2a7b4c3f7c51ce4d7347bdbc3dd1e78a2908c423
Vinesh Kannan <vingkan@gmail.com>
2015-09-15 00:02:53 -0500

    Moved getDeviceIP() to utils.js

--- utils.js
+++ utils.js
@@ -66,3 +66,20 @@ function toggle(){
 		submitButton.style.background = 'transparent';
 	}
 }
+
+function getDeviceIP() {
+    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+    xmlhttp.send();
+
+    hostipInfo = xmlhttp.responseText.split("\n");
+
+    for (i=0; hostipInfo.length >= i; i++) {
+        ipAddress = hostipInfo[i].split(":");
+        if ( ipAddress[0] == "IP" ) return ipAddress[1];
+    }
+
+    return false;
+}
