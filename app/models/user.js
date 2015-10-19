eb1edc2b0b53810aa25a1450dec409c1ea2ada60
Vinesh Kannan <vingkan@gmail.com>
2015-10-19 17:13:25 -0500

    Added utils.js

--- app/models/user.js
+++ app/models/user.js
@@ -3,3 +3,15 @@ User.prototype.coordinates = {
 	'latitude': 0.0,
 	'longitude': 0.0
 }; //Object of Doubles/Floats
+
+User.prototype.getImgLetter = function(){
+	var imgLetter = null;
+	var firstChar = this.name.charAt(0).toLowerCase();
+	if(isLetter(firstChar)){
+		imgLetter = firstChar;
+	}
+	else{
+		imgLetter = 'etc';
+	}
+	return imgLetter;
+}
