447705b8dc10e695af8ad2e7d64adaa29ba2570f
Vinesh Kannan <vingkan@gmail.com>
2015-10-22 11:04:34 -0500

    Removed assignUpdateUser()

--- app/models/database.js
+++ app/models/database.js
@@ -1,10 +1,3 @@
-function assignUpdate(user){
-	setInterval(function(){
-		updateUser(user);
-		console.log('Updating ' + user.name);
-	}, 15000);
-}
-
 function getUsers(){
 	var users = [];
 	var userDatabase = new Firebase('https://circleup.firebaseio.com/users');
