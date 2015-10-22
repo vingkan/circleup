9524c3330ae29724c5022716d426d7d01df36a60
Vinesh Kannan <vingkan@gmail.com>
2015-10-22 11:37:13 -0500

    Added geoSuccess, geoError, geoOptions

--- app/lib/geolocation.js
+++ app/lib/geolocation.js
@@ -10,10 +10,24 @@ function updateCoords(position){
 	userLocation.coordinates.longitude = position.coords.longitude;
 }
 
-navigator.geolocation.getCurrentPosition(function(position){
+/*navigator.geolocation.getCurrentPosition(function(position){
 	updateCoords(position);
-});
+	console.log('getCurrentPosition');
+});*/
 
-var watchUser = navigator.geolocation.watchPosition(function(position){
+var geoSuccess = function(position){
 	updateCoords(position);
-});
+	console.log('watchPosition');
+}
+
+var geoError = function(){
+	console.log('Error on geolocation position.');
+}
+
+var geoOptions = {
+	enableHighAccuracy: true,
+	maximumAge: 30000,
+	timeout: 5000
+}
+
+var watchUser = navigator.geolocation.watchPosition(geoSuccess, geoError, geoOptions);
