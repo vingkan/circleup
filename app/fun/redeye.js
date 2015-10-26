647ca3e5337868beac26851d82f2cc597c37a56c
Vinesh Kannan <vingkan@gmail.com>
2015-10-26 01:04:33 -0500

    Switch to objects model

--- app/fun/redeye.js
+++ app/fun/redeye.js
@@ -12,5 +12,60 @@ function newRedEye(){
 	redeye.getImgLetter = function(){
 		return 'redeye';
 	}
-	addUserMarker(redeye, true);
+	addUsers([redeye]);
+	addRedEyeBox(redeye, true);
+}
+
+function addRedEyeBox(user, allowEdits){
+	var allowDrag = allowEdits || false;
+	var current = user;
+	var markerIcon = {
+		url: 'style/markers/' + current.getImgLetter() + '.png',
+		scaledSize: new google.maps.Size(82.5, 125),
+		origin: new google.maps.Point(0, 0),
+		anchor: new google.maps.Point(41, 125)
+	};
+	var marker = new google.maps.Marker({
+		title: current.name,
+		map: googleMap,
+		position: {
+			lat: current.getLat(),
+			lng: current.getLon()
+		},
+		icon: markerIcon,
+		animation: google.maps.Animation.DROP,
+		draggable: allowDrag
+	});
+	if(allowDrag){
+		marker.addListener('dragend', function(marker, event){
+			updateRedEyePosition(marker, event);
+		});
+	}
+	google.maps.event.addListener(marker, 'click', (function(marker, markerCount){
+		return function(){
+			/*infoWindow.setContent(htmlInfo);
+			infoWindow.open(googleMap, marker);*/
+		}
+	})(marker, markerCount));
+}
+
+function updateRedEyePosition(marker, event){
+	var newPosition = {
+		latitude: marker.latLng.lat(),
+		longitude: marker.latLng.lng(),
+		accuracy: 100
+	}
+	updateRedEye(userLocation, newPosition);
+}
+
+function updateRedEye(user, position){
+	var userDatabase = new Firebase('https://circleup.firebaseio.com/users');
+	navigator.geolocation.getCurrentPosition(updateCoords);
+	var newLocation = position || userLocation.coordinates;
+	userDatabase.child(user.id).update({
+		latitude: newLocation.latitude,
+		longitude: newLocation.longitude,
+		accuracy: newLocation.accuracy
+	});
+	//getUsers();
 }
