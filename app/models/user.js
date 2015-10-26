6864dd439cd27bb54ef174e5a82b9e02df71b05d
Vinesh Kannan <vingkan@gmail.com>
2015-10-25 22:17:15 -0500

    Added form styles

--- app/models/user.js
+++ app/models/user.js
@@ -8,6 +8,7 @@ function currentUserMarker(marker){
 }
 
 function login(){
+	document.getElementById('shade').style.display = 'block';
 	//var userEmail = document.getElementById('loginEmail');
 	var userEmail = 'jChiu@uic.edu';
 	userLocation.email = userEmail;
